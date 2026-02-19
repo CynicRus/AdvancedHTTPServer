fbb1d550c9a69d5d2eeb66101dfec1d10f912b79
Aleksandr Vorobiev <CynicRus@gmail.com>
2026-02-19 17:24:27 +0300

    Refactor AdvancedSessionManager with new features
    Add compability with advanced http router

--- src/AdvancedSessionManager.pas
+++ src/AdvancedSessionManager.pas
@@ -1,3 +1,32 @@
+{
+  Copyright (c) 2026 Aleksandr Vorobev aka CynicRus, CynicRus@gmail.com
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this
+     list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+  3. Neither the name of the copyright holder nor the names of its
+     contributors may be used to endorse or promote products derived from
+     this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+}
+
 unit AdvancedSessionManager;
 
 {$mode objfpc}{$H+}
@@ -9,7 +38,8 @@ interface
 
 uses
   SysUtils, DateUtils, SyncObjs, HashMap,
-  AdvancedHTTPServer;
+  AdvancedHTTPServer,
+  AdvancedHTTPRouter;
 
 type
   TSessionSameSite = TResponseWriter.TCookieSameSite;
@@ -68,8 +98,11 @@ type
   public
   type
     TSessionObj = specialize TSession<TData>;
-    TGetSessionFunc = reference to function(W: TResponseWriter;
-      R: TRequest): TSessionObj;
+
+    TGetSessionFunc = reference to function(W: TResponseWriter; R: TRequest): TSessionObj;
+
+    TGetSessionRouterFunc = reference to function(Ctx: THTTPRouterContext): TSessionObj;
+    TRouterMiddleware = reference to procedure(C: TObject);
   private
   type
     TSessionRec = record
@@ -98,14 +131,12 @@ type
     function MakeAbsTimeoutDT(const NowDT: TDateTime): TDateTime; inline;
     function MakeIdleTimeoutDT(const NowDT: TDateTime): TDateTime; inline;
 
-    function GetAbsExpFromData(const Data: TData; out AbsExpUTC: TDateTime): boolean;
-      inline;
+    function GetAbsExpFromData(const Data: TData; out AbsExpUTC: TDateTime): boolean; inline;
     procedure SetAbsExpToData(var Data: TData; const AbsExpUTC: TDateTime); inline;
 
     procedure EnsureDefaultIDSources;
     function ReadSessionID(R: TRequest): string;
-    procedure WriteSessionID(W: TResponseWriter; const ID: string;
-      MaxAgeSec: integer; Expire: boolean);
+    procedure WriteSessionID(W: TResponseWriter; const ID: string; MaxAgeSec: integer; Expire: boolean);
     function NewID: string;
 
     function IsExpired(const Rec: TSessionRec; const NowDT: TDateTime): boolean; inline;
@@ -128,13 +159,13 @@ type
     procedure Save(W: TResponseWriter; const Sess: TSessionObj);
     procedure DestroySession(W: TResponseWriter; const Sess: TSessionObj);
 
-    procedure Regenerate(W: TResponseWriter; const Sess: TSessionObj;
-      PreserveData: boolean = True);
+    procedure Regenerate(W: TResponseWriter; const Sess: TSessionObj; PreserveData: boolean = True);
     procedure Reset(W: TResponseWriter; const Sess: TSessionObj);
 
     // Middleware: wraps handler and autosaves before finish.
-    function Middleware(const Next: THandlerFunc;
-      out GetSession: TGetSessionFunc): THandlerFunc;
+    function Middleware(const Next: THandlerFunc; out GetSession: TGetSessionFunc): THandlerFunc;
+
+    function RouterMiddleware(out GetSession: TGetSessionRouterFunc): TRouterMiddleware;
   end;
 
 function SessionConfigDefault: TSessionConfig;
@@ -677,7 +708,50 @@ begin
       Sess.Free;
     end;
   end;
+end;
 
+
+function TSessionManager.RouterMiddleware(out GetSession: TGetSessionRouterFunc): TRouterMiddleware;
+var
+  SelfPtr: Pointer;
+begin
+  SelfPtr := Self;
+
+  GetSession := function(Ctx: THTTPRouterContext): TSessionObj
+  begin
+    if Ctx = nil then Exit(nil);
+    Result := TSessionManager(SelfPtr).Get(Ctx.W, Ctx.R);
+  end;
+
+  Result := procedure(C: TObject)
+  var
+    Ctx: THTTPRouterContext;
+    Sess: TSessionObj;
+    Mgr: TSessionManager;
+  begin
+    if C = nil then Exit;
+    Ctx := THTTPRouterContext(C);
+    Mgr := TSessionManager(SelfPtr);
+
+    Sess := Mgr.Get(Ctx.W, Ctx.R);
+
+    Ctx.W.AddOnBeforeFinish(procedure(WW: TResponseWriter; RR: TRequest)
+    begin
+      if Sess <> nil then
+      begin
+        if Sess.Destroyed then
+          Mgr.DestroySession(WW, Sess)
+        else
+          Mgr.Save(WW, Sess);
+      end;
+    end);
+
+    try
+      Ctx.Next;
+    finally
+      Sess.Free;
+    end;
+  end;
 end;
 
 end.
